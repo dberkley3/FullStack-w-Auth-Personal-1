258e203daeaa37cbebf10aeef4ecc7aeb0ca6e4c
dberkley3 <dberkley3@gmail.com>
2022-05-31 03:26:49 -0400

    FullStack w/ Auth Wish List initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = { //exporting an object
+
+    'url' : 'mongodb+srv://wish:list@cluster0.pfqg7.mongodb.net/?retryWrites=true&w=majority', 
+    'dbName': 'messages'
+};
